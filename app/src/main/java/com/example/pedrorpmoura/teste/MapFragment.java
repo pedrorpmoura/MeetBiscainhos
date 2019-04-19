0e845b592ae243ee5ce06f54e046825638e38384
fpereira029 <filipepereira029@gmail.com>
2019-04-19 02:26:20 +0100

    Interactive Map update.

--- app/src/main/java/com/example/pedrorpmoura/teste/MapFragment.java
+++ app/src/main/java/com/example/pedrorpmoura/teste/MapFragment.java
@@ -11,7 +11,10 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.List;
 
 
 /**
@@ -20,6 +23,8 @@ import android.widget.ImageView;
 public class MapFragment extends Fragment implements View.OnTouchListener {
 
     private FragmentTransaction mFragmentTransaction;
+
+    private List<MRoom> rooms = new CreateRooms().getRooms();
 
     public MapFragment() {
         // Required empty public constructor
@@ -30,23 +35,40 @@ public class MapFragment extends Fragment implements View.OnTouchListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View root_view = inflater.inflate(R.layout.fragment_map, container, false);
+        final View root_view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        ImageView iv = (ImageView) root_view.findViewById (R.id.image);
+        final ImageView iv = (ImageView) root_view.findViewById (R.id.image);
+        final ImageView iv2 = (ImageView) root_view.findViewById (R.id.image_areas);
+
         if (iv != null) {
             iv.setOnTouchListener (this);
         }
+
+        final Button piso = (Button) root_view.findViewById(R.id.piso);
+
+        piso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentText = piso.getText().toString();
+                if(currentText.equals("2º Piso")){
+                    iv.setImageResource(R.drawable.oratorio1);       //mudar isto
+                    iv2.setImageResource(R.drawable.oratorio1);      //mudar isto
+                    piso.setText("1º Piso");
+                }else{
+                    iv.setImageResource(R.drawable.original2);
+                    iv2.setImageResource(R.drawable.colored2);
+                    piso.setText("2º Piso");
+                }
+
+            }
+        });
+
 
         return root_view;
     }
 
     public boolean onTouch (View v, MotionEvent ev)
     {
-        MRoom entrada = new MRoom(1,"Sala de Entrada","Inserir descrição", new int[] {}, R.raw.sound_sample);
-        MRoom cavalaricas = new MRoom(8,"Cavalariças", "Inserir descrição",
-                new int[] {}, R.raw.sound_sample);
-        MRoom cozinha = new MRoom(9,"Cozinha", "Inserir descrição",
-                new int[] {}, R.raw.sound_sample);
 
         final int action = ev.getAction();
 
@@ -68,7 +90,8 @@ public class MapFragment extends Fragment implements View.OnTouchListener {
 
             */
 
-            case MotionEvent.ACTION_UP :
+            case MotionEvent.ACTION_UP:
+
                 int touchColor = getHotspotColor (R.id.image_areas, evX, evY);
 
                 ColorTool ct = new ColorTool ();
@@ -76,20 +99,46 @@ public class MapFragment extends Fragment implements View.OnTouchListener {
 
                 if (ct.closeMatch (Color.RED, touchColor, tolerance)){
                     RoomFragment room_fragment = new RoomFragment();
-                    room_fragment.setRoom(entrada);
-                    createFragment(room_fragment, entrada.getName());
+                    for(MRoom r : rooms) {
+                        if(r.getName().equals("Sala de Entrada")) {
+                            room_fragment.setRoom(r);
+                            createFragment(room_fragment, r.getName());
+                            break;
+                        }
+                    }
                 }
 
                 else if (ct.closeMatch (Color.MAGENTA, touchColor, tolerance)){
                     RoomFragment room_fragment = new RoomFragment();
-                    room_fragment.setRoom(cozinha);
-                    createFragment(room_fragment, cozinha.getName());
+                    for(MRoom r : rooms) {
+                        if(r.getName().equals("Cozinha")) {
+                            room_fragment.setRoom(r);
+                            createFragment(room_fragment, r.getName());
+                            break;
+                        }
+                    }
                 }
 
                 else if (ct.closeMatch (Color.YELLOW, touchColor, tolerance)){
                     RoomFragment room_fragment = new RoomFragment();
-                    room_fragment.setRoom(cavalaricas);
-                    createFragment(room_fragment, cavalaricas.getName());
+                    for(MRoom r : rooms) {
+                        if(r.getName().equals("Cavalariças")) {
+                            room_fragment.setRoom(r);
+                            createFragment(room_fragment, r.getName());
+                            break;
+                        }
+                    }
+                }
+
+                else if (ct.closeMatch (Color.GREEN, touchColor, tolerance)){
+                    RoomFragment room_fragment = new RoomFragment();
+                    for(MRoom r : rooms) {
+                        if(r.getName().equals("Jardins")) {
+                            room_fragment.setRoom(r);
+                            createFragment(room_fragment, r.getName());
+                            break;
+                        }
+                    }
                 }
 
                 v.performClick();
